6e513e0180484230547719845fde4b9fcdaea092
Igor Rinkovec <shiqqqi@gmail.com>
2016-05-22 22:51:25 +0200

    Port fix.

--- server.js
+++ server.js
@@ -99,32 +99,6 @@ app.get('/data/:trackId', function(req, res) {
     });
 });
 
-
-// === WEBSOCKET SERVER ===
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-
-
-app.get('/door-open', function(req, res){
-  io.sockets.emit('event',{type:'door-open'});
-  console.log('Door open event!')
-  res.json({"success": "true"});
-});
-
-app.get('/special-door-open', function(req, res){
-  io.sockets.emit('event',{type:'forceSpecialDoorOpen'});
-  console.log('Special door open event!')
-  res.json({"success": "true"});
-});
-
-app.get('/refresh', function(req, res){
-  io.sockets.emit('event',{type:'refresh'});
-  console.log('Refresh event!')
-  res.json({"success": "true"});
+http.listen(3005, function(){
+  console.log('listening on *:3005');
 });
